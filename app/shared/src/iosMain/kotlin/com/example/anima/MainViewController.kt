5dd00f4a45401a6f32a4d073edde7b9856c689b2
Igor <PauloMinoso@proton.me>
2026-08-15 22:25:36 -0300

    initial commit

---
+++ app/shared/src/iosMain/kotlin/com/example/anima/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.anima
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ core/src/commonMain/kotlin/com/example/anima/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.anima
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
