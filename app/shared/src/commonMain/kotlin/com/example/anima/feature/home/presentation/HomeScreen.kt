803b9afd579afa39186058f8ae0971a359eb49ed
Mateusparca <mateusparcianello2@gmail.com>
2026-08-18 16:42:48 -0300

    refactor: removed useless imports on HomeScreen

--- app/shared/src/commonMain/kotlin/com/example/anima/feature/home/presentation/HomeScreen.kt
+++ app/shared/src/commonMain/kotlin/com/example/anima/feature/home/presentation/HomeScreen.kt
@@ -1,25 +1,14 @@
 package com.example.anima.feature.home.presentation
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import com.example.anima.theme.AnimaTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.anima.feature.events.presentation.EventCard
 import com.example.anima.feature.events.presentation.EventCardUiModel
 
 @Composable
