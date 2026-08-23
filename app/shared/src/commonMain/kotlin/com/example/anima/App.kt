12bf6ffc48cfcc3dfecf7fda446b58ff9e9e9bea
gabriel-picoli <vnvsgabe@gmail.com>
2026-08-22 23:49:36 -0300

    feat(screen): add login screen

--- app/shared/src/commonMain/kotlin/com/example/anima/App.kt
+++ app/shared/src/commonMain/kotlin/com/example/anima/App.kt
@@ -1,59 +1,14 @@
 package com.example.anima
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
-
-import anima.app.shared.generated.resources.Res
-import anima.app.shared.generated.resources.compose_multiplatform
 import com.example.anima.core.theme.AnimaTheme
-import anima.app.shared.generated.resources.login_title
+import com.example.anima.features.auth.presentation.login.LoginScreen
+
 @Composable
 @Preview
 fun App() {
     AnimaTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(AnimaTheme.colors.background)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = stringResource(Res.string.login_title),
-                style = AnimaTheme.typography.headlineLarge,
-                color = AnimaTheme.colors.onBackground
-            )
-            Button(onClick = { showContent = !showContent }) {
-                Text(
-                    "Click me!", style = AnimaTheme.typography.headlineLarge,
-                )
-            }
-
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        LoginScreen()
     }
 }
