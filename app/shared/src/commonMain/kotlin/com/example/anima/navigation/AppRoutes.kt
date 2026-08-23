c90b4eba343a5fe30fac6547ee5656181e4db3c5
gabriel-picoli <vnvsgabe@gmail.com>
2026-08-23 16:51:18 -0300

    feat(app): add app routes object

---
+++ app/shared/src/commonMain/kotlin/com/example/anima/navigation/AppRoutes.kt
@@ -0,0 +1,10 @@
+package com.example.anima.navigation
+
+object AppRoutes {
+    const val AUTH_GRAPH = "auth"
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+
+    const val APP_GRAPH = "app"
+    const val HOME = "home"
+}
