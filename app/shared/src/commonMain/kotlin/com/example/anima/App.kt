721cc6c9dd597d27a56e1bc43da3057075819991
gabriel-picoli <vnvsgabe@gmail.com>
2026-08-23 16:53:02 -0300

    feat(app): add nav host in app

--- app/shared/src/commonMain/kotlin/com/example/anima/App.kt
+++ app/shared/src/commonMain/kotlin/com/example/anima/App.kt
@@ -1,51 +1,19 @@
 package com.example.anima
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import org.jetbrains.compose.resources.painterResource
-
-import anima.app.shared.generated.resources.Res
-import anima.app.shared.generated.resources.compose_multiplatform
+import androidx.navigation.compose.rememberNavController
 import com.example.anima.core.theme.AnimaTheme
+import com.example.anima.navigation.AppNavHost
+import com.example.anima.navigation.AppRoutes
 
 @Composable
-@Preview
 fun App() {
     AnimaTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(AnimaTheme.colors.background)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text(
-                    "Click me!", style = AnimaTheme.typography.headlineLarge,
-                )
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        val navController = rememberNavController()
+
+        AppNavHost(
+            navController = navController,
+            startDestination = AppRoutes.AUTH_GRAPH,
+        )
     }
 }
