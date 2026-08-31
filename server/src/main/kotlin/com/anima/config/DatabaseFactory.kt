bd073459b49743d1738c1418314340a89dd6b007
Igor <PauloMinoso@proton.me>
2026-08-31 12:49:09 -0300

    fix(server): fixed docker support for postgres database and server

--- server/src/main/kotlin/com/anima/config/DatabaseFactory.kt
+++ server/src/main/kotlin/com/anima/config/DatabaseFactory.kt
@@ -7,11 +7,15 @@ object DatabaseFactory {
     lateinit var emf: EntityManagerFactory
 
     fun init() {
-        // Get credentials from .env file, as the persistence.xml file cannot access .env directly
+        val host = System.getenv("POSTGRES_HOST") ?: "localhost"
+        val db = requireNotNull(System.getenv("POSTGRES_DB")) { "POSTGRES_DB não definida" }
+        val user = requireNotNull(System.getenv("POSTGRES_USER")) { "POSTGRES_USER não definida" }
+        val password = requireNotNull(System.getenv("POSTGRES_PASSWORD")) { "POSTGRES_PASSWORD não definida" }
+
         val overrides = mapOf(
-            "jakarta.persistence.jdbc.url" to System.getenv("POSTGRES_URI"),
-            "jakarta.persistence.jdbc.user" to System.getenv("POSTGRES_USER"),
-            "jakarta.persistence.jdbc.password" to System.getenv("POSTGRES_PASSWORD"),
+            "jakarta.persistence.jdbc.url" to "jdbc:postgresql://$host:5432/$db",
+            "jakarta.persistence.jdbc.user" to user,
+            "jakarta.persistence.jdbc.password" to password,
             "jakarta.persistence.jdbc.driver" to "org.postgresql.Driver"
         )
         emf = Persistence.createEntityManagerFactory("anima", overrides)
