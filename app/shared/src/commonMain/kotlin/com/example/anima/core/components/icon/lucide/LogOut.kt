af79c6f65aa0f31bdd72aa39c4924e1330a2ef6f
Mateusparca <mateusparcianello2@gmail.com>
2026-09-11 17:02:32 -0300

    feat(core): add new icons for profile

---
+++ app/shared/src/commonMain/kotlin/com/example/anima/core/components/icon/lucide/LogOut.kt
@@ -0,0 +1,61 @@
+package com.example.anima.core.components.icon.lucide
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val LucideLogOut: ImageVector
+    @Composable
+    get() = remember {
+        ImageVector.Builder(
+            name = "LucideLogOut",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f,
+        ).apply {
+            // path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"
+            path(
+                stroke = SolidColor(Color.Black),
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+            ) {
+                moveTo(9f, 21f)
+                horizontalLineTo(5f)
+                arcToRelative(2f, 2f, 0f, false, true, -2f, -2f)
+                verticalLineTo(5f)
+                arcToRelative(2f, 2f, 0f, false, true, 2f, -2f)
+                horizontalLineToRelative(4f)
+            }
+
+            // path d="m16 17 5-5-5-5"
+            path(
+                stroke = SolidColor(Color.Black),
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+            ) {
+                moveTo(16f, 17f)
+                lineToRelative(5f, -5f)
+                lineToRelative(-5f, -5f)
+            }
+
+            // path d="M21 12H9"
+            path(
+                stroke = SolidColor(Color.Black),
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+            ) {
+                moveTo(21f, 12f)
+                horizontalLineTo(9f)
+            }
+        }.build()
+    }
